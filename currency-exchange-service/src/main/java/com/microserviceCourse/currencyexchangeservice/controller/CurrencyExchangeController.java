c0f19b5813a9bd326fdc104179ac4582903fd07d
urkin <urkin961@gmail.com>
2024-01-23 12:07:18 +0100

    Added distributed tracing server (Zipkin) to monitor query tracing.

--- currency-exchange-service/src/main/java/com/microserviceCourse/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/microserviceCourse/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.microserviceCourse.currencyexchangeservice.controller;
 
 import com.microserviceCourse.currencyexchangeservice.enities.CurrencyRate;
 import com.microserviceCourse.currencyexchangeservice.repository.CurrencyExchangeRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +18,9 @@ public class CurrencyExchangeController {
     private Environment environment;
     private CurrencyExchangeRepository repository;
 
+    private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+
+
     @Autowired
     public CurrencyExchangeController(Environment environment, CurrencyExchangeRepository repository) {
         this.environment = environment;
@@ -24,6 +29,7 @@ public class CurrencyExchangeController {
 
     @GetMapping("/currency-exchange/{from}/{to}")
     public CurrencyRate retrieveCurrency(@PathVariable String from, @PathVariable String to) throws SQLException {
+        logger.info("Method retrieveCurrency was called with {} to {}", from, to);
         CurrencyRate USDtoRUBrate = repository.findByFromAndTo(from, to);
         if (USDtoRUBrate == null) {
             throw new RuntimeException("Unable to find currency rate for " + from + " to " + to);
